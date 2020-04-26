10abed436c33c908e96a2a1899d131c1e1fb52b1
haianhdt98 <phamhaianhdt98@gmail.com>
2020-04-26 15:30:38 +0700

    create polls

---
+++ aiohttpdemo_polls/main.py
@@ -0,0 +1,26 @@
+# aiohttpdemo_polls/main.py
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+
+from settings import config, BASE_DIR
+from routes import setup_routes
+from db import close_pg, init_pg
+from middlewares import setup_middlewares
+
+
+app = web.Application()
+
+app['config'] = config
+
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates')))
+
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
+
+setup_routes(app)
+
+setup_middlewares(app)
+
+web.run_app(app)
